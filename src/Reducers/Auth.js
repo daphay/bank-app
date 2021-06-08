a9887f1392be45d8143fec48123ada109d5a9581
daphay <dafizo1@gmail.com>
2021-06-08 13:15:42 +0100

    added auth file

---
+++ src/Reducers/Auth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SIGN_IN, SIGN_OUT}  from '../Utils/Constants';
+
+
+const AuthReducer = (state = {}, action) =>{
+  switch(action.type){
+    case SIGN_IN:
+        return action.user;
+    case SIGN_OUT:
+       return {};
+    default:
+        return state;
+  }
+};
+
+export default AuthReducer;
