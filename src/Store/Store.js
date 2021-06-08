cbe1ae66db7d127d770419c4cdff5b1017771413
daphay <dafizo1@gmail.com>
2021-06-08 18:08:59 +0100

    created a redux store with a single auth reducer

---
+++ src/Store/Store.js
@@ -0,0 +1,16 @@
+
+import {createStore, combineReducers,applyMiddleware,compose} from 'redux';
+import AuthReducer from '../Reducers/Auth';
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||compose;
+
+const store = createStore(
+    combineReducers({
+        auth: AuthReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
